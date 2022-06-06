986db2583d831e1d46df10816e60652b23e0555b
Matthew Wu <mnfwu12@gmail.com>
2022-06-06 14:11:10 -0400

    update default with gl_FragCoord

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -3,9 +3,13 @@ precision mediump float;
 #endif
 
 uniform float uTime;
+uniform vec2 uResolution;
 
 void main() {
-    vec3 color = vec3(1.0, abs(sin(uTime)), 1.0);
+    vec2 st = gl_FragCoord.xy/uResolution.xy;
+
+    vec3 color = vec3(0.3, 0.2, 1.0);
+    color.r += st.x / 2.0;
 
     gl_FragColor = vec4(color, 1.0);
 }
